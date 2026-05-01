c3ed1ac5a8bcc24fd907cffb4604403b694c1e0e
Rudr <ankitgautam50366@gmail.com>
2026-05-01 23:57:38 +0530

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,15 @@
+'''
+12345
+1234
+123
+12
+1
+'''
+
+def pattern(n):
+    for i in range(1 , n+1 ):
+        for j in range(1 , n+2-i):
+            print(j , end="")
+        print()
+
+pattern(5)
