5ff9610e2a56387afa1f647cb94245c66891836d
Rudr <ankitgautam50366@gmail.com>
2026-09-01 18:47:32 +0530

    Time: 12 ms (55.72%) | Memory: 19.3 MB (76.16%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -3,18 +3,13 @@ class Solution:
         if len(s) != len(t):
             return False
 
-        count = {}
+        countS = {}
+        countT = {}
 
         for char in s:
-            count[char] = count.get(char, 0) + 1
+            countS[char] = countS.get(char, 0) + 1
 
         for char in t:
-            if char not in count:
-                return False
+            countT[char] = countT.get(char, 0) + 1
 
-            count[char] -= 1
-
-            if count[char] < 0:
-                return False
-
-        return True
+        return countS == countT
